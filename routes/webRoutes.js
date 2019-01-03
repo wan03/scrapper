580080c0cbd66837e4fffdabc1c60da6bee6d1ab
wan03 <38962452+wan03@users.noreply.github.com>
2019-01-03 01:20:37 -0500

    Started on routes and handlebars.

--- routes/webRoutes.js
+++ routes/webRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const webController = require("../controllers/webController");
 
 const userController = require("../controllers/webController");
 
-router.get;
+router.get("/", webController.home);
+
+router.get("/products", webController.products);
+
+router.post("/products", webController.addComment);
 
 module.exports = router;
