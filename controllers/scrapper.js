4830a422b45bdd9752560fb2ac608f76bca87beb
wan03 <38962452+wan03@users.noreply.github.com>
2019-01-08 11:18:59 -0500

    Adjusting to deploy to google cloud

--- controllers/scrapper.js
+++ controllers/scrapper.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 module.exports = async () => {
   const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    args: ["--no-sandbox"]
   });
   const page = await browser.newPage();
   await page.goto(
